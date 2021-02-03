0f9f21a1236639401b073f5600ab4a1aeb9d3e27
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-02-03 23:24:16 +0200

    practice

---
+++ app.js
@@ -0,0 +1,40 @@
+// let tr = prompt("введите знак триугольника");
+// let tr2 = "";
+// let nam = prompt('Введите какой динны должен быть триугольник')
+// for(let i = 0; i<= nam; i++){
+//     tr2 += tr;
+//     console.log(tr2);
+// }
+
+// let nam = 0;
+// for(let i = 0; i<=60; i++){
+//     nam+=1
+//     if(nam% 5 == 0 && nam% 3 == 0){
+//         let nam35 = "FizzBuzz"
+//         nam += 1
+//         console.log(nam35)
+//     }if(nam% 3 == 0){
+//         let nam3 = "Fizz"
+//         nam += 1
+//         console.log(nam3)
+//     }
+//     if(nam% 5 == 0){
+//         let nam5 = "Buzz"
+//         nam += 1
+//         console.log(nam5)
+//     }
+    
+
+//     console.log(nam)
+// } 
+
+
+// let size = 8;
+// let t = "# # # #"
+// let r =" # # # # "
+// for(let i = 5; i<=size; i++){
+//     console.log(t)
+//     console.log(r)
+// }
+
+
