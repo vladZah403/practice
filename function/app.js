d8c1f8e969007e6626ad9636894806723642511e
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-07-18 21:08:57 +0300

    countBs

--- function/app.js
+++ function/app.js
@@ -8,4 +8,14 @@
  
 // console.log(min(3,1))
 
+function countBs(string){
+    
 
+  let str = string.split('B').length -1; 
+
+console.log(`в слове ${string} ${str} B`)
+  
+}
+
+
+console.log(countBs('BalBd'))
